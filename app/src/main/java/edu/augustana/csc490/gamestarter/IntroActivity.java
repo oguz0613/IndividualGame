d7a3b7ed99dd8c5b31aa8cc6740a0f4fa70f35cf
oguz0613 <omarguz0613@gmail.com>
2015-03-30 15:00:43 -0500

    Initial Commit

---
+++ app/src/main/java/edu/augustana/csc490/gamestarter/IntroActivity.java
@@ -0,0 +1,60 @@
+package edu.augustana.csc490.gamestarter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class IntroActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Button launcher = (Button) findViewById(R.id.buttonLaunch);
+        launcher.setOnClickListener(clickHandler);
+
+
+    }
+
+    View.OnClickListener clickHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+
+            startActivity(intent);
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_intro, menu);
+        return true;
+
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/edu/augustana/csc490/gamestarter/MainGameView.java
@@ -0,0 +1,198 @@
+// CannonView.java
+// Displays and controls the Cannon Game
+package edu.augustana.csc490.gamestarter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MainGameView extends SurfaceView implements SurfaceHolder.Callback
+{
+    private static final String TAG = "GameStarter"; // for Log.w(TAG, ...)
+
+    private GameThread gameThread; // runs the main game loop
+    private Activity mainActivity; // keep a reference to the main Activity
+
+    private boolean isGameOver = true;
+
+    private int x;
+    private int y;
+    private int screenWidth;
+    private int screenHeight;
+
+    private Paint myPaint;
+    private Paint backgroundPaint;
+
+    public MainGameView(Context context, AttributeSet atts)
+    {
+        super(context, atts);
+        mainActivity = (Activity) context;
+
+        getHolder().addCallback(this);
+
+        myPaint = new Paint();
+        myPaint.setColor(Color.BLUE);
+        backgroundPaint = new Paint();
+        backgroundPaint.setColor(Color.CYAN);
+    }
+
+    // called when the size changes (and first time, when view is created)
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh)
+    {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        screenWidth = w;
+        screenHeight = h;
+
+        startNewGame();
+    }
+
+    public void startNewGame()
+    {
+        this.x = 25;
+        this.y = 25;
+
+        if (isGameOver)
+        {
+            isGameOver = false;
+            gameThread = new GameThread(getHolder());
+            gameThread.start(); // start the main game loop going
+        }
+    }
+
+
+    private void gameStep()
+    {
+        x++;
+    }
+
+    public void updateView(Canvas canvas)
+    {
+        if (canvas != null) {
+            canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), backgroundPaint);
+            canvas.drawCircle(x, y, 20, myPaint);
+        }
+    }
+
+    // stop the game; may be called by the MainGameFragment onPause
+    public void stopGame()
+    {
+        if (gameThread != null)
+            gameThread.setRunning(false);
+    }
+
+    // release resources; may be called by MainGameFragment onDestroy
+    public void releaseResources()
+    {
+        // release any resources (e.g. SoundPool stuff)
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    // called when the surface is destroyed
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
+        // ensure that thread terminates properly
+        boolean retry = true;
+        gameThread.setRunning(false); // terminate gameThread
+
+        while (retry)
+        {
+            try
+            {
+                gameThread.join(); // wait for gameThread to finish
+                retry = false;
+            }
+            catch (InterruptedException e)
+            {
+                Log.e(TAG, "Thread interrupted", e);
+            }
+        }
+    }
+
+        @Override
+    public boolean onTouchEvent(MotionEvent e)
+    {
+        if (e.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            this.x = (int) e.getX();
+            this.y = (int) e.getY();
+        }
+
+        return true;
+    }
+
+    // Thread subclass to run the main game loop
+    private class GameThread extends Thread
+    {
+        private SurfaceHolder surfaceHolder; // for manipulating canvas
+        private boolean threadIsRunning = true; // running by default
+
+        // initializes the surface holder
+        public GameThread(SurfaceHolder holder)
+        {
+            surfaceHolder = holder;
+            setName("GameThread");
+        }
+
+        // changes running state
+        public void setRunning(boolean running)
+        {
+            threadIsRunning = running;
+        }
+
+        @Override
+        public void run()
+        {
+            Canvas canvas = null;
+
+            while (threadIsRunning)
+            {
+                try
+                {
+                    // get Canvas for exclusive drawing from this thread
+                    canvas = surfaceHolder.lockCanvas(null);
+
+                    // lock the surfaceHolder for drawing
+                    synchronized(surfaceHolder)
+                    {
+                        gameStep();         // update game state
+                        updateView(canvas); // draw using the canvas
+                    }
+                    Thread.sleep(10); // if you want to slow down the action...
+                } catch (InterruptedException ex) {
+                    Log.e(TAG,ex.toString());
+                }
+                finally  // regardless if any errors happen...
+                {
+                    // make sure we unlock canvas so other threads can use it
+                    if (canvas != null)
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+        }
+    }
+}
